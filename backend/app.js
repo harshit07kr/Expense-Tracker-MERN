90854f4e9016a51077e8fb5c577e94d274d3e558
HARSHIT <harshitkr9435@gmail.com>
2024-07-23 13:18:19 +0530

    vercel

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,41 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/db');
-const {readdirSync} = require('fs')
+const { readdirSync } = require('fs');
 const cookieParser = require('cookie-parser');
-const app = express()
+const path = require('path');
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
-
-//middlewares
-app.use(express.json())
+// Middlewares
+app.use(express.json());
 app.use(
-    cors({
-      origin: true,
-      credentials: true,
-    })
-  );
-  
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
+// Routes
+readdirSync('./routes').map((route) => app.use('/api', require('./routes/' + route)));
 
-//routes
-readdirSync('./routes').map((route) => app.use('/', require('./routes/' + route)))
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
+  db();
+  app.listen(PORT, () => {
+    console.log('listening to port:', PORT);
+  });
+};
 
-server()
+server();
