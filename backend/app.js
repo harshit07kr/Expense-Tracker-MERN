53d706fe65bb31f9109eb9a0b2e05a80c15ce05d
HARSHIT <harshitkr9435@gmail.com>
2024-07-23 13:55:18 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,35 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
-const { readdirSync } = require('fs');
+const {readdirSync} = require('fs')
 const cookieParser = require('cookie-parser');
-const path = require('path');
-const app = express();
+const app = express()
 
-require('dotenv').config();
+require('dotenv').config()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 
-// Middlewares
-app.use(express.json());
+
+//middlewares
+app.use(express.json())
 app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+  
 app.use(cookieParser());
 
-// Routes
-readdirSync('./routes').map((route) => app.use('/', require('./routes/' + route)));
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../build')));
-
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+//routes
+readdirSync('./routes').map((route) => app.use('/', require('./routes/' + route)))
 
 const server = () => {
-  db();
-  app.listen(PORT, () => {
-    console.log('listening to port:', PORT);
-  });
-};
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
 
-server();
+server()
