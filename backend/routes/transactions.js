1cbbb6a80af6ef242c24ac5b1df8807cc7fdbd25
HARSHIT <harshitkr9435@gmail.com>
2024-08-10 18:08:25 +0530

    minor changes

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -4,11 +4,11 @@ const requireAuth = require('../middleware/auth')
 const router = require('express').Router();
 
 
-router.post('/add-income', requireAuth, addIncome)
-    .get('/get-incomes', requireAuth, getIncomes)
-    .delete('/delete-income/:id', requireAuth, deleteIncome)
-    .post('/add-expense', requireAuth, addExpense)
-    .get('/get-expenses', requireAuth, getExpense)
-    .delete('/delete-expense/:id', requireAuth, deleteExpense)
+router.post(`/add-income`, requireAuth, addIncome)
+    .get(`/get-incomes`, requireAuth, getIncomes)
+    .delete(`/delete-income/:id`, requireAuth, deleteIncome)
+    .post(`/add-expense`, requireAuth, addExpense)
+    .get(`/get-expenses`, requireAuth, getExpense)
+    .delete(`/delete-expense/:id`, requireAuth, deleteExpense)
 
 module.exports = router
